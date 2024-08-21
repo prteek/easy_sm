9c35a985a288dc39e2657dfb56bdb99ed0f18ee2
prteek <prteek@icloud.com>
2024-08-21 21:48:15 +0100

    adding provision to read and write files during local processing and make operations

--- easy_sm/template/easy_sm_base/local_test/make_local.sh
+++ easy_sm/template/easy_sm_base/local_test/make_local.sh
@@ -7,4 +7,4 @@ target=$4
 aws_profile=$5
 aws_region=$6
 
-docker run -v ~/.aws:/root/.aws -e AWS_PROFILE=${aws_profile} -e AWS_DEFAULT_REGION=${aws_region} --rm "${image}:${tag}" make ${target}
+docker run -v ~/.aws:/root/.aws -v ${test_path}:/opt/ml -e AWS_PROFILE=${aws_profile} -e AWS_DEFAULT_REGION=${aws_region} --rm "${image}:${tag}" make ${target}
