b9cbc8a598ea7393631998548aea26ca99d88ba7
prteek <prteek@icloud.com>
2024-08-25 15:35:40 +0100

    Option to specify number of  instances on Sagemaker

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ Recommended to validate before updating.
     """,
     author='Prateek',
     author_email='prteek@icloud.com',
-    version="0.1.5",
+    version="0.1.6",
     python_requires='>=3.11',
     packages=find_packages(where='.'),
     package_data={
