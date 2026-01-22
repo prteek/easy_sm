a458ea54cbcdc566bd8ab27c4fb230cd70ab3625
prteek <prteek@icloud.com>
2026-01-22 20:14:39 +0000

    fix: change docker run -it to -d in deploy_local.sh for non-interactive usage

--- easy_sm/template/easy_sm_base/local_test/deploy_local.sh
+++ easy_sm/template/easy_sm_base/local_test/deploy_local.sh
@@ -4,4 +4,4 @@ test_path=$1
 tag=$2
 image=$3
 
-docker run -it -v ${test_path}:/opt/ml -p 8080:8080 --rm "${image}:${tag}" serve
+docker run -d -v ${test_path}:/opt/ml -p 8080:8080 --rm "${image}:${tag}" serve
