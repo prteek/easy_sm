fe09e1e13f66c83589a088fd50bd50242560e761
prteek <prteek@icloud.com>
2026-02-06 15:22:13 +0000

    chore: prepare v1.0.0 release
    - Bump version to 1.0.0 in setup.py
    - Add __version__ to easy_sm/__init__.py
    - Update .gitignore to exclude build artifacts (site/, dist/, .coverage)

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ Recommended to validate before updating.
     """,
     author="Prateek",
     author_email="prteek@icloud.com",
-    version="0.2.0",
+    version="1.0.0",
     python_requires=">=3.13",
     packages=find_packages(where="."),
     package_data={
