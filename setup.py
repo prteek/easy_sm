c395a85be0ee29d597721db35625ac69899953e7
prteek <prteek@icloud.com>
2026-07-07 14:35:35 +0100

    chore: bump version to 1.1.0
    - Add environment variables support for processing jobs
    - Upgrade sagemaker to 2.257.3
    - Update documentation with new features

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ Recommended to validate before updating.
     """,
     author="Prateek",
     author_email="prteek@icloud.com",
-    version="1.0.4",
+    version="1.1.0",
     python_requires=">=3.13",
     packages=find_packages(where="."),
     package_data={
